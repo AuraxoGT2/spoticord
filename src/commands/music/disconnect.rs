63fb1d42b80d30dbf748052e7b522169e5e97116
AuraxoGT2 <166641017+AuraxoGT2@users.noreply.github.com>
2025-07-18 19:47:34 +0300

    Update disconnect.rs

--- src/commands/music/disconnect.rs
+++ src/commands/music/disconnect.rs
@@ -3,69 +3,70 @@ use poise::CreateReply;
 use serenity::all::CreateEmbed;
 use spoticord_session::manager::SessionQuery;
 use spoticord_utils::discord::Colors;
-
 use crate::bot::Context;
 
-// highlight-start
-#[poise::command(
-    slash_command,
-    guild_only,
-    name = "atsijungti",
-    description = "Atjungia botą iš balso kanalo."
-)]
+
+
+#[poise::command(slash_command, guild_only)]
 pub async fn disconnect(ctx: Context<'_>) -> Result<(), Error> {
-// highlight-end
-    let manager = ctx.data();
-    let guild = ctx.guild_id().expect("poise lied to me");
+    let manager = ctx.data();
+    let guild = ctx.guild_id().expect("poise lied to me");
 
-    let Some(session) = manager.get_session(SessionQuery::Guild(guild)) else {
-        ctx.send(
-            CreateReply::default()
-                .embed(
-                    CreateEmbed::new()
-                        .title("Negaliu atsijungti")
-                        // highlight-next-line
-                        .description("Aš neprisijungęs prie jokio balso kanalo.")
-                        .color(Colors::Error),
-                )
-                .ephemeral(true),
-        )
-        .await?;
 
-        return Ok(());
-    };
+    let Some(session) = manager.get_session(SessionQuery::Guild(guild)) else {
+        ctx.send(
+            CreateReply::default()
+               .embed(
+                    CreateEmbed::new()
+                       .title("Negaliu atsijungti")
+                        .description("Aš nesu jokiame kanale.")
+                        .color(Colors::Error),
+                )
+                .ephemeral(true),
+        )
+        .await?;
 
-    if session.active().await? && session.owner().await? != ctx.author().id {
-        ctx.send(
-            CreateReply::default()
-                .embed(
-                    CreateEmbed::new()
-                        // highlight-start
-                        .title("Veiksmas negalimas")
-                        .description("Atjungti botą gali tik seansą pradėjęs narys.")
-                        // highlight-end
-                        .color(Colors::Error),
-                )
-                .ephemeral(true),
-        )
-        .await?;
 
-        return Ok(());
-    }
+        return Ok(());
+    };
 
-    session.disconnect().await;
 
-    ctx.send(
-        CreateReply::default().embed(
-            CreateEmbed::new()
-                // highlight-start
-                .title("Atsijungiau!")
-                .description("Sėkmingai atsijungiau. Naudokite `/prisijungti`, kad vėl mane pakviestumėte.")
-                // highlight-end
-                .color(Colors::Info),
-        ),
-    )
-    .await?;
 
-    Ok(())
+    if session.active().await? && session.owner().await? != ctx.author().id {
+        ctx.send(
+            CreateReply::default()
+                .embed(
+                    CreateEmbed::new()
+                       .title("Negaliu atsijungti")
+                        .description("Tik muzikos leidėjas gali atlikti šį veiksmą.")
+                        .color(Colors::Error),
+                )
+                .ephemeral(true),
+        )
+        .await?;
+
+
+
+        return Ok(());
+    }
+
+
+
+    session.disconnect().await;
+
+
+
+    ctx.send(
+        CreateReply::default().embed(
+            CreateEmbed::new()
+                .title("Atsijungimas!")
+                .description("Atsijungta, naudokite /join komandą kad prisijungčiau.")
+                .color(Colors::Info),
+
+        ),
+    )
+ .await?;
+
+
+    Ok(())
 }
